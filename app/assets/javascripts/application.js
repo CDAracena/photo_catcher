9fe46ae961430fc6b3535d8c65d92b9297d5e7e1
Christian Aracena <CDAracena@gmail.com>
2018-08-02 22:29:56 -0400

    set up devise for user registration and imported bulma css

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,5 +10,7 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
+//= require jquery/dist/jquery.min.js
+//= require bulma-toast/dist/js/bulma-toast.js
 //= require rails-ujs
 //= require_tree .
--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -1,2 +1,35 @@
 class PhotosController < ApplicationController
+  def index
+
+  end
+
+  def create
+
+  end
+
+  def new
+
+  end
+
+  def edit
+
+  end
+
+  def show
+
+  end
+
+  def update
+
+  end
+
+  def destroy
+
+  end
+
+  private
+
+  def photo_params
+    params.require(:photo).permit(:title, :description)
+  end
 end
